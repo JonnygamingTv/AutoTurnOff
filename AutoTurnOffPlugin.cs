2596f8c583a31b8ad30c77a0912663965b2dd369
JGTV <22498524+JonnygamingTv@users.noreply.github.com>
2025-12-23 22:36:29 +0100

    Add files via upload

--- AutoTurnOffPlugin.cs
+++ AutoTurnOffPlugin.cs
@@ -14,7 +14,7 @@ namespace Pustalorc.Plugins.AutoTurnOff
     {
         protected override void Load()
         {
-            U.Events.OnPlayerDisconnected += Disconnected;
+            if (BarricadeManager.regions != null) U.Events.OnPlayerDisconnected += Disconnected;
             Logger.Log("Auto Turn Off has been loaded!");
         }
 
@@ -26,46 +26,46 @@ namespace Pustalorc.Plugins.AutoTurnOff
 
         private void Disconnected([NotNull] UnturnedPlayer player)
         {
-            var playerId = player.CSteamID.m_SteamID;
+            Rocket.Core.Utils.TaskDispatcher.RunAsync(() => {
+                ulong playerId = player.CSteamID.m_SteamID;
 
-            if (BarricadeManager.regions == null)
-                return;
+                var buildables = BarricadeManager.regions.Cast<BarricadeRegion>().Concat(BarricadeManager.vehicleRegions)
+                    .SelectMany(k => k.drops)
+                    .Select(k =>
+                        !Configuration.Instance.ItemsToDisable.Contains(k.asset.id) ||
+                        k.GetServersideData().owner != playerId
+                            ? null
+                            : k.interactable).Where(k => k != null).ToList();
 
-            var buildables = BarricadeManager.regions.Cast<BarricadeRegion>().Concat(BarricadeManager.vehicleRegions)
-                .SelectMany(k => k.drops)
-                .Select(k =>
-                    !Configuration.Instance.ItemsToDisable.Contains(k.asset.id) ||
-                    k.GetServersideData().owner != playerId
-                        ? null
-                        : k.interactable).Where(k => k != null).ToList();
-
-            foreach (var interactable in buildables)
-                switch (interactable)
-                {
-                    case InteractableSafezone saf:
-                        BarricadeManager.ServerSetSafezonePowered(saf, false);
-                        break;
-                    case InteractableOxygenator oxy:
-                        BarricadeManager.ServerSetOxygenatorPowered(oxy, false);
-                        break;
-                    case InteractableSpot spot:
-                        BarricadeManager.ServerSetSpotPowered(spot, false);
-                        break;
-                    case InteractableGenerator gen:
-                        BarricadeManager.ServerSetGeneratorPowered(gen, false);
-                        break;
-                    case InteractableFire fire:
-                        BarricadeManager.ServerSetFireLit(fire, false);
-                        break;
-                    case InteractableOven oven:
-                        BarricadeManager.ServerSetOvenLit(oven, false);
-                        break;
-                    case InteractableStereo stereo:
-                        BarricadeManager.ServerSetStereoTrack(stereo, Guid.Empty);
-                        break;
-                }
-
-            Logger.Log($"Turned off {buildables.Count} barricades.");
-        }
+                foreach (var interactable in buildables)
+                    switch (interactable)
+                    {
+                        case InteractableSafezone saf:
+                            Rocket.Core.Utils.TaskDispatcher.QueueOnMainThread(()=>BarricadeManager.ServerSetSafezonePowered(saf, false));
+                            break;
+                        case InteractableOxygenator oxy:
+                            Rocket.Core.Utils.TaskDispatcher.QueueOnMainThread(()=>BarricadeManager.ServerSetOxygenatorPowered(oxy, false));
+                            break;
+                        case InteractableSpot spot:
+                            Rocket.Core.Utils.TaskDispatcher.QueueOnMainThread(()=>BarricadeManager.ServerSetSpotPowered(spot, false));
+                            break;
+                        case InteractableGenerator gen:
+                            Rocket.Core.Utils.TaskDispatcher.QueueOnMainThread(()=>BarricadeManager.ServerSetGeneratorPowered(gen, false));
+                            break;
+                        case InteractableFire fire:
+                            Rocket.Core.Utils.TaskDispatcher.QueueOnMainThread(()=>BarricadeManager.ServerSetFireLit(fire, false));
+                            break;
+                        case InteractableOven oven:
+                            Rocket.Core.Utils.TaskDispatcher.QueueOnMainThread(()=>BarricadeManager.ServerSetOvenLit(oven, false));
+                            break;
+                        case InteractableStereo stereo:
+                            Rocket.Core.Utils.TaskDispatcher.QueueOnMainThread(()=>BarricadeManager.ServerSetStereoTrack(stereo, Guid.Empty));
+                            break;
+                    }
+#if DEBUG
+                Logger.Log("Turned off " + buildables.Count + " barricades.");
+#endif
+            });
+        }    
     }
 }
